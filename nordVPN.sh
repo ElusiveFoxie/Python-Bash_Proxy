86195ac435bb98e868a69f40c85a73090bf558b8
ElusiveFox <44260690+ElusiveFoxie@users.noreply.github.com>
2020-06-08 13:55:32 +0200

    Update nordVPN.sh

--- nordVPN.sh
+++ nordVPN.sh
@@ -2,7 +2,7 @@
 
 # Bash Script for using NordVPN servers as proxies for request.py
 
-for k in pl al cr us gr lv ch pt ch ar hr hk lu ro tw au cy hu my rs th at cz is mx tr be dk md sk ua ba ee nl si uk br 'in' it fi ie nz za bg fr il mk kr vn ca ge no es cl de jp se sg
+for k in pl al cr us gr lv ch pt ch ar hr hk lu ro tw au cy hu my rs th at cz is mx tr be dk md sk ua ba ee nl si uk br 'in' it 'fi' ie nz za 'bg' fr il mk kr vn ca ge no es cl de jp se sg
 do
   for i in {0..2999}
 	do
